accb6e3a41066105b4c469aa8c45ca90b5e02e73
Shruthi5ch <138290030+Shruthi5ch@users.noreply.github.com>
2024-07-29 23:47:06 +0530

    Add files via upload

---
+++ pass_gen.py
@@ -0,0 +1,8 @@
+import random
+import string
+def generate_password(length:int=10):
+    alphabet=  string.punctuation + string.digits + string.ascii_letters 
+    password= ''.join(random.choice(alphabet) for i in range(length))
+    return password
+password = generate_password()
+print(f"Generate password: {password}")
